3762dae9fa2a231152792b65d569519b3f23367b
Gabriel Joseph <gabrieljoseph@Gabriels-MBP.fios-router.home>
2021-04-28 14:18:28 -0400

    updated session controller to remove pry

--- Backend/app/controllers/sessions_controller.rb
+++ Backend/app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
     include CurrentUserConcern
 
     def create
-        binding.pry
         @user = User.find_by(email: params[:email])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
